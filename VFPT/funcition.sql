de66b5dd3b85c3375db1b9fead99253c4f8417e9
Archanjo09 <filipearchanjo09@gmail.com>
2026-06-14 19:51:10 -0300

    funcition.sql

--- VFPT/funcition.sql
+++ VFPT/funcition.sql
@@ -31,24 +31,3 @@ BEGIN
     RETURN ROUND(p_preco_venda * v_taxa_percentual, 2);
 END;
 $$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION fn_log_transacao_mercado()
-RETURNS TRIGGER AS $$
-DECLARE
-    v_id_item_base INT;
-BEGIN
-    SELECT id_item_base INTO v_id_item_base
-    FROM inventario_usuario
-    WHERE id = OLD.id_inventario_item;
-
-    INSERT INTO mercado_historico (id, id_item_base, valor_venda, data_venda)
-    VALUES (
-        COALESCE((SELECT MAX(id) FROM mercado_historico), 0) + 1,
-        v_id_item_base,
-        OLD.preco_venda,
-        CURRENT_TIMESTAMP
-    );
-    
-    RETURN OLD;
-END;
-$$ LANGUAGE plpgsql;
